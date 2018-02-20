ecd96fbc5293fb0a8e3b58287ca7ad37ab4849ec
trevor-richardson <trevorwoodsrichardson@gmail.com>
2018-02-19 20:41:10 -0700

    updated current scene to include multi ball scenario

--- vrep_scripts/run_vrep_simulation.py
+++ vrep_scripts/run_vrep_simulation.py
@@ -17,11 +17,18 @@ base_dir = config['DEFAULT']['BASE_DIR']
 
 '''
 This is my main script to generate data from my vrep scene. This collects the video data and the collision ground truth
-
 '''
 
-x_list_of_positions = np.random.normal(0, 1.2, 30000)
-y_list_of_positions = np.random.normal(-8 , 1.2, 30000)
+x_list_of_positions = np.random.normal(0, 1.0, 3000)
+y_list_of_positions = np.random.normal(-12 , 1.0, 3000)
+x_list_of_positions0 = np.random.normal(1, 1.0, 3000)
+y_list_of_positions0 = np.random.normal(-12 , 1.0, 3000)
+x_list_of_positions1 = np.random.normal(-1, 1.0, 3000)
+y_list_of_positions1 = np.random.normal(-12 , 1.0, 3000)
+x_list_of_positions2 = np.random.normal(2, 1.0, 3000)
+y_list_of_positions2 = np.random.normal(-12 , 1.0, 3000)
+x_list_of_positions3 = np.random.normal(-2, 1.0, 3000)
+y_list_of_positions3 = np.random.normal(-12 , 1.0, 3000)
 z_permanent = .2555
 
 with open(base_dir + '/vrep_scripts/saved_vel_pos_data/current_position.txt', "w") as new_pos_file:
@@ -29,15 +36,31 @@ with open(base_dir + '/vrep_scripts/saved_vel_pos_data/current_position.txt', "w
     print(y_list_of_positions[0], file=new_pos_file)
     print(z_permanent, file=new_pos_file)
 
+with open(base_dir + '/vrep_scripts/saved_vel_pos_data/current_position0.txt', "w") as new_pos_file:
+    print(x_list_of_positions0[1], file=new_pos_file)
+    print(y_list_of_positions0[1], file=new_pos_file)
+    print(z_permanent, file=new_pos_file)
 
+with open(base_dir + '/vrep_scripts/saved_vel_pos_data/current_position1.txt', "w") as new_pos_file:
+    print(x_list_of_positions1[2], file=new_pos_file)
+    print(y_list_of_positions1[2], file=new_pos_file)
+    print(z_permanent, file=new_pos_file)
+
+with open(base_dir + '/vrep_scripts/saved_vel_pos_data/current_position2.txt', "w") as new_pos_file:
+    print(x_list_of_positions2[3], file=new_pos_file)
+    print(y_list_of_positions2[3], file=new_pos_file)
+    print(z_permanent, file=new_pos_file)
+
+with open(base_dir + '/vrep_scripts/saved_vel_pos_data/current_position3.txt', "w") as new_pos_file:
+    print(x_list_of_positions3[4], file=new_pos_file)
+    print(y_list_of_positions3[4], file=new_pos_file)
+    print(z_permanent, file=new_pos_file)
 
 def start():
     vrep.simxFinish(-1) # just in case, close all opened connections
     clientID=vrep.simxStart('127.0.0.1',19997,True,True,5000,5) #start my Connection
-    #x =vrep.simxStopSimulation(clientID,vrep.simx_opmode_oneshot)
     error_code =vrep.simxStartSimulation(clientID,vrep.simx_opmode_oneshot_wait)
     return clientID, error_code
-
 
 def collectImageData(clientID):
     #get my vision sensor working and print the data in the while loop below for 5 seconds after simulation begins
@@ -57,9 +80,9 @@ def collectImageData(clientID):
         while (vrep.simxGetConnectionId(clientID)!=-1 and time.time() < t_end):
             res,resolution,image=vrep.simxGetVisionSensorImage(clientID,v0,0,vrep.simx_opmode_buffer)
 
-            if count % 20 == 0:
-                action = np.random.randint(-4, high=4)
-                action *=5
+            if count % 100 == 0:
+                action = np.random.randint(-5, high=5)
+                action *=10
                 return_val = vrep.simxSetJointTargetVelocity(clientID, left_handle, action, vrep.simx_opmode_oneshot)
                 return_val2 = vrep.simxSetJointTargetVelocity(clientID, right_handle, action, vrep.simx_opmode_oneshot_wait)
 
@@ -78,13 +101,7 @@ def collectImageData(clientID):
                 rotate_img = np.flipud(img)
                 list_of_images.append(rotate_img)
 
-                # scipy.misc.imsave(str(count) + 'outfile.png', rotate_img)
                 count+=1
-
-                #The following plays a video of the new image
-                # cv2.imshow('image',rotate_img)
-                # if cv2.waitKey(1) & 0xFF == ord('q'):
-                #     break
         return list_of_images, collector
     else:
         sys.exit()
@@ -110,32 +127,6 @@ def detectCollisionSignal(clientID):
     else:
         # print ("\nMiss")
         return 0
-
-# def detectCollisionSignalPerterbDistance(clientID):
-#     #get the current position of the robot
-#     lineTracerBase=vrep.simxGetObjectHandle(clientID, "LineTracerBase", vrep.simx_opmode_oneshot_wait)
-#     ret_code, pos=vrep.simxGetObjectPosition(clientID, lineTracerBase[1], -1, vrep.simx_opmode_oneshot_wait)
-#
-#     if (abs(pos[0] - .057) > .07):
-#         return 1 #there is a hit
-#     else:
-#         return 0
-
-# def detectCollisionSignalPerterbAngle(clientID):
-#
-#     lineTracerBase=vrep.simxGetObjectHandle(clientID, "LineTracerBase", vrep.simx_opmode_oneshot_wait)
-#     ret_code, pos=vrep.simxGetObjectOrientation(clientID, lineTracerBase[1], -1, vrep.simx_opmode_oneshot_wait)
-#     if abs(pos[2]) > .01:
-#         return 1 # there is a hit
-#     else:
-#         if abs(pos[0]) > .01:
-#             return 1
-#         else:
-#             if abs(pos[1]) > .1:
-#                 return 1
-#             else:
-#                 return 0
-#     return 0
 
 
 def writeImagesStatesToFiles(image_array, state_array, n_iter, collision_signal):
@@ -235,6 +226,10 @@ def writeImagesStatesToFiles(image_array, state_array, n_iter, collision_signal)
 
 def write_to_hit_miss_txt(n_iter, collision_signal, txt_file_counter):
     filename_newpos = base_dir + '/vrep_scripts/saved_vel_pos_data/current_position.txt'
+    filename_newpos0 = base_dir + '/vrep_scripts/saved_vel_pos_data/current_position0.txt'
+    filename_newpos1 = base_dir + '/vrep_scripts/saved_vel_pos_data/current_position1.txt'
+    filename_newpos2 = base_dir + '/vrep_scripts/saved_vel_pos_data/current_position2.txt'
+    filename_newpos3 = base_dir + '/vrep_scripts/saved_vel_pos_data/current_position3.txt'
     filename_miss = base_dir + '/vrep_scripts/saved_vel_pos_data/train/miss/miss' + str(txt_file_counter)
     filename_hit = base_dir + '/vrep_scripts/saved_vel_pos_data/train/hit/hit' + str(txt_file_counter)
     filename_get_velocity = base_dir + '/vrep_scripts/saved_vel_pos_data/velocity.txt'
@@ -261,6 +256,22 @@ def write_to_hit_miss_txt(n_iter, collision_signal, txt_file_counter):
         print(x_list_of_positions[txt_file_counter + 1], file=new_pos_file)
         print(y_list_of_positions[txt_file_counter + 1], file=new_pos_file)
         print(z_permanent, file=new_pos_file)
+    with open(filename_newpos0, "w") as new_pos_file:
+        print(x_list_of_positions0[txt_file_counter + 1], file=new_pos_file)
+        print(y_list_of_positions0[txt_file_counter + 1], file=new_pos_file)
+        print(z_permanent, file=new_pos_file)
+    with open(filename_newpos1, "w") as new_pos_file:
+        print(x_list_of_positions1[txt_file_counter + 1], file=new_pos_file)
+        print(y_list_of_positions1[txt_file_counter + 1], file=new_pos_file)
+        print(z_permanent, file=new_pos_file)
+    with open(filename_newpos2, "w") as new_pos_file:
+        print(x_list_of_positions2[txt_file_counter + 1], file=new_pos_file)
+        print(y_list_of_positions2[txt_file_counter + 1], file=new_pos_file)
+        print(z_permanent, file=new_pos_file)
+    with open(filename_newpos3, "w") as new_pos_file:
+        print(x_list_of_positions3[txt_file_counter + 1], file=new_pos_file)
+        print(y_list_of_positions3[txt_file_counter + 1], file=new_pos_file)
+        print(z_permanent, file=new_pos_file)
 
 def single_simulation(n_iter, txt_file_counter):
     print("####################################################################################################################")
@@ -279,7 +290,7 @@ def single_simulation(n_iter, txt_file_counter):
     print("\n")
 
 def main(iter_start, iter_end):
-    txt_file_counter = 20000
+    txt_file_counter = 10
     for current_iteration in range(iter_start, iter_end):
         single_simulation(current_iteration, txt_file_counter)
         txt_file_counter+=1
